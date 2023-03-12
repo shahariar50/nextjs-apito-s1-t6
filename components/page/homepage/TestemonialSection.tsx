14889800c3d5624e17535add1efcdfeff4c83b79
shahariar50 <shahariar.touhid@gmail.com>
2023-03-12 21:03:47 +0600

    setup testemonial slider

--- components/page/homepage/TestemonialSection.tsx
+++ components/page/homepage/TestemonialSection.tsx
@@ -1,3 +1,6 @@
+import { Autoplay, Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
 const TestemonialSection = () => {
   return (
     <section className="py-8" id="testimonial">
@@ -26,182 +29,166 @@ const TestemonialSection = () => {
           }}
         ></div>
 
-        <div
-          className="carousel slide"
-          id="carouselExampleDark"
-          data-bs-ride="carousel"
+        <Swiper
+          modules={[Navigation, Autoplay, Pagination]}
+          autoplay
+          spaceBetween={0}
+          slidesPerView={1}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          navigation={{
+            prevEl: ".carousel-control-prev",
+            nextEl: ".carousel-control-next",
+          }}
+          pagination
         >
-          <div className="carousel-inner">
-            <div className="carousel-item active" data-bs-interval="10000">
-              <div className="row h-100">
-                <div className="col-12">
-                  <div className="card text-white bg-primary-gradient">
-                    <div className="card-body p-4 p-md-4 p-lg-7">
-                      <h2 className="fw-bold text-white text-center">
-                        What our customer are saying
-                      </h2>
-                      <hr
-                        className="mx-auto"
-                        style={{ height: "2px", width: "50px" }}
-                      />
-                      <div className="d-md-flex align-items-md-center mt-5 text-center text-md-start">
-                        <picture>
-                          <img
-                            className="img-fluid me-4 me-md-3 me-lg-4"
-                            src="/img/gallery/user-1.png"
-                            width="100"
-                            alt=""
-                          />
-                        </picture>
-                        <div className="w-md-25 my-3">
-                          <h5 className="mb-0 fw-medium text-white">
-                            Edward Newgate
-                          </h5>
-                          <p className="fw-normal mb-0">Founder Circle</p>
-                        </div>
-                        <div className="w-md-75">
-                          <p className="card-text ms-md-5">
-                            “Our dedicated patient engagement app and web portal
-                            allow you to access information instantaneously (no
-                            tedeous form, long calls, or administrative hassle)
-                            and securely”
-                          </p>
-                        </div>
+          <SwiperSlide>
+            <div className="row h-100">
+              <div className="col-12">
+                <div className="card text-white bg-primary-gradient">
+                  <div className="card-body p-4 p-md-4 p-lg-7">
+                    <h2 className="fw-bold text-white text-center">
+                      What our customer are saying
+                    </h2>
+                    <hr
+                      className="mx-auto"
+                      style={{ height: "2px", width: "50px" }}
+                    />
+                    <div className="d-md-flex align-items-md-center mt-5 text-center text-md-start">
+                      <picture>
+                        <img
+                          className="img-fluid me-4 me-md-3 me-lg-4"
+                          src="/img/gallery/user-1.png"
+                          width="100"
+                          alt=""
+                        />
+                      </picture>
+                      <div className="w-md-25 my-3">
+                        <h5 className="mb-0 fw-medium text-white">
+                          Edward Newgate
+                        </h5>
+                        <p className="fw-normal mb-0">Founder Circle</p>
+                      </div>
+                      <div className="w-md-75">
+                        <p className="card-text ms-md-5">
+                          “Our dedicated patient engagement app and web portal
+                          allow you to access information instantaneously (no
+                          tedeous form, long calls, or administrative hassle)
+                          and securely”
+                        </p>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="carousel-item" data-bs-interval="2000">
-              <div className="row h-100">
-                <div className="col-12">
-                  <div className="card text-white bg-primary-gradient">
-                    <div className="card-body p-4 p-md-4 p-lg-7">
-                      <h2 className="fw-bold text-white text-center">
-                        Our dear customers said about us
-                      </h2>
-                      <hr
-                        className="mx-auto"
-                        style={{ height: "2px", width: "50px" }}
-                      />
-                      <div className="d-md-flex align-items-md-center mt-5 text-center text-md-start">
-                        <picture>
-                          <img
-                            className="img-fluid me-4 me-md-3 me-lg-4"
-                            src="/img/gallery/user-2.png"
-                            width="100"
-                            alt=""
-                          />
-                        </picture>
-                        <div className="w-md-25 my-3">
-                          <h5 className="mb-0 fw-medium text-white">
-                            Jhon Doe
-                          </h5>
-                          <p className="fw-normal mb-0">UI/UX Designer</p>
-                        </div>
-                        <div className="w-md-75">
-                          <p className="card-text ms-md-5">
-                            “Our dedicated patient engagement app and web portal
-                            allow you to access information instantaneously (no
-                            tedeous form, long calls, or administrative hassle)
-                            and securely”
-                          </p>
-                        </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="row h-100">
+              <div className="col-12">
+                <div className="card text-white bg-primary-gradient">
+                  <div className="card-body p-4 p-md-4 p-lg-7">
+                    <h2 className="fw-bold text-white text-center">
+                      Our dear customers said about us
+                    </h2>
+                    <hr
+                      className="mx-auto"
+                      style={{ height: "2px", width: "50px" }}
+                    />
+                    <div className="d-md-flex align-items-md-center mt-5 text-center text-md-start">
+                      <picture>
+                        <img
+                          className="img-fluid me-4 me-md-3 me-lg-4"
+                          src="/img/gallery/user-2.png"
+                          width="100"
+                          alt=""
+                        />
+                      </picture>
+                      <div className="w-md-25 my-3">
+                        <h5 className="mb-0 fw-medium text-white">Jhon Doe</h5>
+                        <p className="fw-normal mb-0">UI/UX Designer</p>
+                      </div>
+                      <div className="w-md-75">
+                        <p className="card-text ms-md-5">
+                          “Our dedicated patient engagement app and web portal
+                          allow you to access information instantaneously (no
+                          tedeous form, long calls, or administrative hassle)
+                          and securely”
+                        </p>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="carousel-item">
-              <div className="row h-100">
-                <div className="col-12">
-                  <div className="card text-white bg-primary-gradient">
-                    <div className="card-body p-4 p-md-4 p-lg-7">
-                      <h2 className="fw-bold text-white text-center">
-                        Our dear customers said about us
-                      </h2>
-                      <hr
-                        className="mx-auto"
-                        style={{ height: "2px", width: "50px" }}
-                      />
-                      <div className="d-md-flex align-items-md-center mt-5 text-center text-md-start">
-                        <picture>
-                          <img
-                            className="img-fluid me-4 me-md-3 me-lg-4"
-                            src="/img/gallery/user-3.png"
-                            width="100"
-                            alt=""
-                          />
-                        </picture>
-                        <div className="w-md-25 my-3">
-                          <h5 className="mb-0 fw-medium text-white">
-                            Jeny Doe
-                          </h5>
-                          <p className="fw-normal mb-0">Web Designer</p>
-                        </div>
-                        <div className="w-md-75">
-                          <p className="card-text ms-md-5">
-                            “Our dedicated patient engagement app and web portal
-                            allow you to access information instantaneously (no
-                            tedeous form, long calls, or administrative hassle)
-                            and securely”
-                          </p>
-                        </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="row h-100">
+              <div className="col-12">
+                <div className="card text-white bg-primary-gradient">
+                  <div className="card-body p-4 p-md-4 p-lg-7">
+                    <h2 className="fw-bold text-white text-center">
+                      Our dear customers said about us
+                    </h2>
+                    <hr
+                      className="mx-auto"
+                      style={{ height: "2px", width: "50px" }}
+                    />
+                    <div className="d-md-flex align-items-md-center mt-5 text-center text-md-start">
+                      <picture>
+                        <img
+                          className="img-fluid me-4 me-md-3 me-lg-4"
+                          src="/img/gallery/user-3.png"
+                          width="100"
+                          alt=""
+                        />
+                      </picture>
+                      <div className="w-md-25 my-3">
+                        <h5 className="mb-0 fw-medium text-white">Jeny Doe</h5>
+                        <p className="fw-normal mb-0">Web Designer</p>
+                      </div>
+                      <div className="w-md-75">
+                        <p className="card-text ms-md-5">
+                          “Our dedicated patient engagement app and web portal
+                          allow you to access information instantaneously (no
+                          tedeous form, long calls, or administrative hassle)
+                          and securely”
+                        </p>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
+          </SwiperSlide>
+        </Swiper>
+        <div className="row mt-4 flex-center navigation">
+          <div className="col-4 col-sm-5 text-end position-relative z-index-2">
+            <span
+              className="carousel-control-prev carousel-icon z-index-2"
+              role="button"
+              data-bs-slide="prev"
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Previous</span>
+            </span>
           </div>
-          <div className="row mt-4 flex-center">
-            <div className="col-4 col-sm-5 text-end position-relative z-index-2">
-              <a
-                className="carousel-control-prev carousel-icon z-index-2"
-                href="#carouselExampleDark"
-                role="button"
-                data-bs-slide="prev"
-              >
-                <span
-                  className="carousel-control-prev-icon"
-                  aria-hidden="true"
-                ></span>
-                <span className="visually-hidden">Previous</span>
-              </a>
-            </div>
-            <div className="col-auto position-relative z-index-2">
-              <ol className="carousel-indicators">
-                <li
-                  className="active"
-                  data-bs-target="#carouselExampleDark"
-                  data-bs-slide-to="0"
-                ></li>
-                <li
-                  data-bs-target="#carouselExampleDark"
-                  data-bs-slide-to="1"
-                ></li>
-                <li
-                  data-bs-target="#carouselExampleDark"
-                  data-bs-slide-to="2"
-                ></li>
-              </ol>
-            </div>
-            <div className="col-4 col-sm-5 position-relative z-index-2">
-              <a
-                className="carousel-control-next carousel-icon z-index-2"
-                href="#carouselExampleDark"
-                role="button"
-                data-bs-slide="next"
-              >
-                <span
-                  className="carousel-control-next-icon"
-                  aria-hidden="true"
-                ></span>
-                <span className="visually-hidden">Next</span>
-              </a>
-            </div>
+          <div className="col-auto position-relative z-index-2"></div>
+          <div className="col-4 col-sm-5 position-relative z-index-2">
+            <span
+              className="carousel-control-next carousel-icon z-index-2"
+              role="button"
+              data-bs-slide="next"
+            >
+              <span
+                className="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Next</span>
+            </span>
           </div>
         </div>
       </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from "next/app";
 import "styles/scss/theme.scss";
+import "swiper/css";
+import "swiper/css/pagination";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
